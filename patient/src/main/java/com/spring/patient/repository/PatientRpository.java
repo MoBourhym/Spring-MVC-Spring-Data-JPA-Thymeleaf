82d69bd1875dd77bbc800fc7143fc62b456d9d62
MoBourhym <simobourhym01@gmail.com>
2025-04-17 14:44:35 +0100

    Find by name query

--- patient/src/main/java/com/spring/patient/repository/PatientRpository.java
+++ patient/src/main/java/com/spring/patient/repository/PatientRpository.java
@@ -1,9 +1,15 @@
 package com.spring.patient.repository;
 
 import com.spring.patient.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PatientRpository extends JpaRepository<Patient, Long> {
 
-
+Page<Patient> findByNomContains(String keyword, Pageable pageable);
+@Query("select p from Patient p where p.nom like :x")
+    Page<Patient> findByNom(@Param("x") String x, Pageable pageable);
 }
