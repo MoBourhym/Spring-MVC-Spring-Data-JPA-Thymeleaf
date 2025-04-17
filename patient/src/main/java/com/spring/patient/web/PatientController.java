1da7d52d05fda0b9b3c65c76f7c81a8f77c00bfe
MoBourhym <simobourhym01@gmail.com>
2025-04-17 14:35:05 +0100

    Patients controller updates

--- patient/src/main/java/com/spring/patient/web/PatientController.java
+++ patient/src/main/java/com/spring/patient/web/PatientController.java
@@ -3,6 +3,8 @@ package com.spring.patient.web;
 
 import com.spring.patient.entities.Patient;
 import com.spring.patient.repository.PatientRpository;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -10,6 +12,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -20,9 +23,12 @@ public class PatientController {
     private PatientRpository patientRpository;
 
     @GetMapping("/index")
-    public String index(Model model) {
-        Page<Patient> patientList = patientRpository.findAll(PageRequest.of(0,4));
-        model.addAttribute("listPatients",patientList.getContent());
+    public String index(Model model , HttpServletRequest request) {
+        int page=Integer.parseInt(request.getParameter("page"));
+        int size=Integer.parseInt(request.getParameter("size"));
+        Page<Patient>  pagePatients = patientRpository.findAll(PageRequest.of(page,size));
+        model.addAttribute("listPatients",pagePatients.getContent());
+        model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
         return "patients";
     }
 
