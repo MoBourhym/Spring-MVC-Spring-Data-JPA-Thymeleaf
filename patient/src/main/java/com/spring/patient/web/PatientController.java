67854c9e1fb250e28549fc0ddced02095c067199
MoBourhym <simobourhym01@gmail.com>
2025-04-17 14:18:21 +0100

    Patients Template

--- patient/src/main/java/com/spring/patient/web/PatientController.java
+++ patient/src/main/java/com/spring/patient/web/PatientController.java
@@ -1,20 +1,28 @@
 package com.spring.patient.web;
 
 
+import com.spring.patient.entities.Patient;
 import com.spring.patient.repository.PatientRpository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
 public class PatientController {
-    @Autowired
+
     private PatientRpository patientRpository;
 
-    @GetMapping("/idex")
-    public String index() {
+    @GetMapping("/index")
+    public String index(Model model) {
+        Page<Patient> patientList = patientRpository.findAll(PageRequest.of(0,4));
+        model.addAttribute("listPatients",patientList.getContent());
         return "patients";
     }
 
